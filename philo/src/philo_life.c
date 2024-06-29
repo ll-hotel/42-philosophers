7c9d620870b4c0e7e56a016fb88d54cce155a5b1
ll-hotel <ll-hotel@student.42lyon.fr>
2024-06-29 10:35:29 +0200

    Add thread killing when initializing fails

--- philo/src/philo_life.c
+++ philo/src/philo_life.c
@@ -6,7 +6,7 @@
 /*   By: ll-hotel <ll-hotel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 00:17:15 by ll-hotel          #+#    #+#             */
-/*   Updated: 2024/06/26 18:34:31 by ll-hotel         ###   ########.fr       */
+/*   Updated: 2024/06/29 10:34:12 by ll-hotel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,45 +15,54 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-void	philo_life(t_philo *self)
+static bool	philo_eat(t_philo *this, t_fork *dead_philo, struct timeval *tv);
+
+
+void	philo_life(t_philo *this)
 {
-	t_fork *const	dead_philo = &self->world->dead_philo;
+	t_fork *const	dead_philo = &this->world->dead_philo;
 	struct timeval	tv[1];
 
-	while (check_fork(dead_philo) != 0)
+	while (check_fork(dead_philo) != 0 && \
+			check_fork(dead_philo) != this->world->philo_number + 1)
 		usleep(100);
-	self->start_time = ms_now(tv);
-	self->last_meal_time = self->start_time;
-	if (self->id % 2)
-		ms_sleep(self, self->world->time_to_eat, &dead_philo->taken);
+	this->start_time = ms_now(tv);
+	this->last_meal_time = this->start_time;
+	if (this->id % 2)
+		ms_sleep(this, this->world->time_to_eat, &dead_philo->taken);
 
-	while (self->meals_left != 0 && !check_fork(dead_philo))
+	while (this->meals_left != 0 && !check_fork(dead_philo))
 	{
-		if (take_fork(self, &dead_philo->taken, self->left))
+		if (!philo_eat(this, dead_philo, tv))
 			continue ;
-		printf("%lu %lu has taken a fork\n", \
-				ms_now(tv) - self->start_time, self->id);
-		if (take_fork(self, &dead_philo->taken, self->right))
+		leave_fork(this->left, 0);
+		leave_fork(this->right, 0);
+		printf("%lu %lu is sleeping\n", ms_now(tv) - this->start_time, this->id);
+		if (ms_sleep(this, this->world->time_to_sleep, &dead_philo->taken))
 			continue ;
-		printf("%lu %lu has taken a fork\n", \
-				ms_now(tv) - self->start_time, self->id);
-		if (check_self_death(self))
-			continue ;
-		self->last_meal_time = ms_now(tv);
-		printf("%lu %lu is eating\n", self->last_meal_time - self->start_time, self->id);
-		if (ms_sleep(self, self->world->time_to_eat, &dead_philo->taken))
-			continue ;
-		self->meals_left -= (self->meals_left > 0);
-
-		leave_fork(self->left, 0);
-		leave_fork(self->right, 0);
-		printf("%lu %lu is sleeping\n", ms_now(tv) - self->start_time, self->id);
-		if (ms_sleep(self, self->world->time_to_sleep, &dead_philo->taken))
-			continue ;
-
-		printf("%lu %lu is thinking\n", ms_now(tv) - self->start_time, self->id);
+		printf("%lu %lu is thinking\n", ms_now(tv) - this->start_time, this->id);
 	}
+	if (this->id == check_fork(dead_philo))
+		printf("%lu %lu died\n", ms_now(tv) - this->start_time, this->id);
+}
 
-	if (self->id == check_fork(dead_philo))
-		printf("%lu %lu died\n", ms_now(tv) - self->start_time, self->id);
+static bool	philo_eat(t_philo *this, t_fork *dead_philo, struct timeval *tv)
+{
+	if (take_fork(this, &dead_philo->taken, this->left))
+		return (false);
+	printf("%lu %lu has taken a fork\n", \
+			ms_now(tv) - this->start_time, this->id);
+	if (take_fork(this, &dead_philo->taken, this->right))
+		return (false);
+	printf("%lu %lu has taken a fork\n", \
+			ms_now(tv) - this->start_time, this->id);
+	if (check_self_death(this))
+		return (false);
+	this->last_meal_time = ms_now(tv);
+	printf("%lu %lu is eating\n", this->last_meal_time - this->start_time, \
+			this->id);
+	if (ms_sleep(this, this->world->time_to_eat, &dead_philo->taken))
+		return (false);
+	this->meals_left -= (this->meals_left > 0);
+	return (true);
 }
--- philo/src/threading.c
+++ philo/src/threading.c
@@ -6,7 +6,7 @@
 /*   By: ll-hotel <ll-hotel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 23:52:13 by ll-hotel          #+#    #+#             */
-/*   Updated: 2024/06/24 05:52:44 by ll-hotel         ###   ########.fr       */
+/*   Updated: 2024/06/29 10:34:33 by ll-hotel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,9 @@ void	threading(t_world *world)
 					(void *)philo_life, &world->philos[i]) != 0)
 		{
 			printf("Error: failed to create thread %lu\n", i);
-			// Kill threads
+			leave_fork(&world->dead_philo, world->philo_number + 1);
+			while (i > 0)
+				pthread_join(world->threads[--i], NULL);
 			return ;
 		}
 		i += 1;
